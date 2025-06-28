067757be070b42104b301bc44f5c5b3342cf2c85
Chenqi <1002652154@qq.com>
2025-06-28 17:33:29 +0200

    add WndDesign dependency

---
+++ src/main.cpp
@@ -0,0 +1,127 @@
+#include <WndDesign/window/Global.h>
+#include <WndDesign/widget/TitleBarFrame.h>
+#include <WndDesign/control/Placeholder.h>
+
+#include <WndDesign/system/d2d_api.h>
+#include <WndDesign/system/directx_helper.h>
+
+#include <hb.h>
+#include <hb-directwrite.h>
+#include <iostream>
+
+
+using namespace WndDesign;
+
+
+struct MainFrameStyle : public TitleBarFrame::Style {
+	MainFrameStyle() {
+		title.text.assign(L"TextTools");
+	}
+};
+
+class GlyphRun {
+private:
+	friend struct GlyphRunFigure;
+	DWRITE_GLYPH_RUN data;
+	std::vector<UINT16> glyph_indices;
+	std::vector<FLOAT> advances;
+	std::vector<DWRITE_GLYPH_OFFSET> offsets;
+public:
+	GlyphRun(ref_ptr<hb_face_t> font_face, float font_size, ref_ptr<hb_buffer_t> buffer) {
+		unsigned int count;
+		hb_glyph_info_t* info = hb_buffer_get_glyph_infos(buffer, &count);
+		hb_glyph_position_t* pos = hb_buffer_get_glyph_positions(buffer, &count);
+		glyph_indices.resize(count);
+		advances.resize(count);
+		offsets.resize(count);
+		for (unsigned i = 0; i < count; ++i) {
+			glyph_indices[i] = static_cast<UINT16>(info[i].codepoint);
+			advances[i] = pos[i].x_advance / 64.0f;
+			offsets[i].advanceOffset = pos[i].x_offset / 64.0f;
+			offsets[i].ascenderOffset = -pos[i].y_offset / 64.0f;
+		}
+		data.fontFace = hb_directwrite_face_get_font_face(font_face);
+		data.fontEmSize = font_size;
+		data.glyphCount = count;
+		data.glyphIndices = glyph_indices.data();
+		data.glyphAdvances = advances.data();
+		data.glyphOffsets = offsets.data();
+		data.isSideways = false;
+		data.bidiLevel = 0;
+	}
+};
+
+struct GlyphRunFigure : Figure {
+	const GlyphRun& glyph_run;
+	Point origin;
+	Color color;
+
+	GlyphRunFigure(const GlyphRun& glyph_run, Point origin, Color color) : glyph_run(glyph_run), origin(origin), color(color) {}
+
+	virtual void DrawOn(RenderTarget& target, Point point) const override {
+		target.DrawGlyphRun(
+			AsD2DPoint(origin),
+			&glyph_run.data,
+			&GetD2DSolidColorBrush(color),
+			DWRITE_MEASURING_MODE_NATURAL
+		);
+	}
+};
+
+class HBFont {
+private:
+	hb_blob_t* blob;
+	hb_face_t* face;
+	hb_font_t* font;
+public:
+	HBFont(const char* file) {
+		blob = hb_blob_create_from_file(file);
+		face = hb_face_create(blob, 0);
+		font = hb_font_create(face);
+	}
+	~HBFont() {
+		hb_font_destroy(font);
+		hb_face_destroy(face);
+		hb_blob_destroy(blob);
+	}
+	hb_face_t* get_face() const { return face; }
+	hb_font_t* get_font() const { return font; }
+};
+
+GlyphRun ShapeText(const HBFont& font, const char* text) {
+	hb_buffer_t* buffer = hb_buffer_create();
+	hb_buffer_add_utf8(buffer, text, -1, 0, -1);
+	hb_buffer_guess_segment_properties(buffer);
+	hb_shape(font.get_font(), buffer, nullptr, 0);
+	GlyphRun glyph_run(font.get_face(), 20, buffer);
+	hb_buffer_destroy(buffer);
+	return glyph_run;
+}
+
+class Canvas : public Placeholder<Assigned, Assigned> {
+public:
+	Canvas() : font("C:\\Windows\\Fonts\\arial.ttf"), glyph_run(ShapeText(font, "Hello, HarfBuzz!")) {}
+protected:
+	Size size;
+protected:
+	virtual Size OnSizeRefUpdate(Size size_ref) override { return size = size_ref; }
+
+protected:
+	HBFont font;
+	GlyphRun glyph_run;
+protected:
+	virtual void OnDraw(FigureQueue& figure_queue, Rect draw_region) override {
+		figure_queue.add(point_zero, new GlyphRunFigure(glyph_run, Point(0, 30), Color(Color::Green, 0x80)));
+	}
+};
+
+
+int main() {
+	global.AddWnd(
+		new TitleBarFrame(
+			MainFrameStyle(),
+			new Canvas()
+		)
+	);
+	global.MessageLoop();
+}
